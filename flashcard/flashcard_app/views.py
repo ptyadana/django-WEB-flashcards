e188054f15a86208a69c22b9ebda31138082f475
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-22 12:53:01 +0800

    finalize division page,view

--- flashcard/flashcard_app/views.py
+++ flashcard/flashcard_app/views.py
@@ -194,4 +194,66 @@ def multiplication(request):
 
 def division(request):
     """division page"""
-    return render(request,'flashcard_app/division.html',{})
+
+    #generate two new random numbers for quizz
+    new_num_1 = randint(0,9)
+    new_num_2 = randint(1,9) #to avoid division by zero
+
+    #check the request method
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        #color scheme for bootstrap alert boxes
+        alert_color = 'danger'
+
+        try:
+            answer = float(answer)
+            old_num_1 = int(old_num_1)
+            old_num_2 = int(old_num_2)
+            
+            #handle showing 1.0 instead of 1
+            if (old_num_1 % old_num_2) == 0:
+                correct_answer = int(old_num_1 / old_num_2)
+            else:
+                correct_answer = round(old_num_1 / old_num_2,2)
+
+            #check the answer and display the result
+            if answer == correct_answer:
+                result = 'Correct! '
+                msg = f'{old_num_1} ÷ {old_num_2} = {correct_answer}'
+                alert_color = 'success'
+
+                return render(request,'flashcard_app/division.html',{
+                    'alert_color':alert_color,
+                    'msg':msg,
+                    'result':result,
+                    'new_num_1':new_num_1,
+                    'new_num_2':new_num_2
+                    })
+            else:
+                result = 'Incorrect! '
+                msg = f'{old_num_1} ÷ {old_num_2} = {correct_answer}'
+                alert_color = 'warning'
+
+                return render(request,'flashcard_app/division.html',{
+                    'alert_color':alert_color,
+                    'msg':msg,
+                    'result':result,
+                    'new_num_1':new_num_1,
+                    'new_num_2':new_num_2
+                    })
+        except ValueError:
+            msg = 'The answer is invalid. Please try again.'
+            return render(request,'flashcard_app/division.html',{
+                'alert_color':alert_color,
+                'msg':msg,
+                'new_num_1':old_num_1,
+                'new_num_2':old_num_2
+                })
+
+    return render(request,'flashcard_app/division.html',{
+        'new_num_1':new_num_1,
+        'new_num_2':new_num_2,
+    })
