0a29d168f593024e3f24fcb4fdf39336035af139
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-22 09:47:32 +0800

    modify templates structure, add add,sub,multiply,divide html,views,url

--- flashcard/flashcard_app/urls.py
+++ flashcard/flashcard_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from flashcard_app import views
 
 urlpatterns = [
-   path('',views.home,name='home')
+   path('',views.home,name='home'),
+   path('addition',views.addition,name='addition'),
+   path('subtraction',views.subtraction,name='subtraction'),
+   path('multiplication',views.multiplication,name='multiplication'),
+   path('division',views.division,name='division'),
 ]
--- flashcard/flashcard_app/views.py
+++ flashcard/flashcard_app/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{})
+    return render(request,'flashcard_app/home.html',{})
+
+def addition(request):
+    return render(request,'flashcard_app/addition.html',{})
+
+def subtraction(request):
+    return render(request,'flashcard_app/subtraction.html',{})
+
+def multiplication(request):
+    return render(request,'flashcard_app/multiplication.html',{})
+
+def division(request):
+    return render(request,'flashcard_app/division.html',{})
