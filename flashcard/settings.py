92d9aad10f96af22345b78ff7c01461c820e2103
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-23 12:59:57 +0800

    update setting

--- flashcard/settings.py
+++ flashcard/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%+=4t6f8$u6#5ecs76+_e9y3op72pnkkklie@+7=nk64oev9c)'
+# SECRET_KEY = '%+=4t6f8$u6#5ecs76+_e9y3op72pnkkklie@+7=nk64oev9c)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
 ALLOWED_HOSTS = []
 
